a38f6d7945e88735478e6ca3b54504c08cd28889
Sławomir <ruudn7@wp.pl>
2021-01-07 22:10:39 +0100

    add ng-conente to external component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() customClasses = '';
+  @Output() clickButton = new EventEmitter<void>();
+
+}
